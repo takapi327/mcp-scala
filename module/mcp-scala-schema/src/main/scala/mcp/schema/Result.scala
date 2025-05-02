3e0dfb4d1c72950d4a48237c41c47c5fc88fba41
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-05-03 01:51:09 +0900

    Replace CompleteResult to result pacakge

--- module/mcp-scala-schema/src/main/scala/mcp/schema/Result.scala
+++ module/mcp-scala-schema/src/main/scala/mcp/schema/Result.scala
@@ -7,7 +7,6 @@
 package mcp.schema
 
 import io.circe.*
-import io.circe.syntax.*
 
 import mcp.schema.result.*
 import mcp.schema.McpSchema.{
@@ -15,30 +14,6 @@ import mcp.schema.McpSchema.{
 }
 
 object Result:
-
-  /**
-   * The server's response to a completion/complete request
-   */
-  final case class CompleteResult(values: List[String], total: Option[Int], hasMore: Option[Boolean]) extends Result
-  object CompleteResult:
-    given Decoder[CompleteResult] = Decoder.instance { cursor =>
-      for {
-        values  <- cursor.get[List[String]]("completion.values")
-        total   <- cursor.get[Option[Int]]("completion.total")
-        hasMore <- cursor.get[Option[Boolean]]("completion.hasMore")
-      } yield CompleteResult(values, total, hasMore)
-    }
-    given Encoder[CompleteResult] = Encoder.instance { result =>
-      Json
-        .obj(
-          "completion" -> Json.obj(
-            "values"  -> result.values.asJson,
-            "total"   -> result.total.asJson,
-            "hasMore" -> result.hasMore.asJson
-          )
-        )
-        .dropNullValues
-    }
 
   /**
    * The client's response to a roots/list request from the server.
---
+++ module/mcp-scala-schema/src/main/scala/mcp/schema/result/CompleteResult.scala
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2025-2026 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package mcp.schema
+package result
+
+import io.circe.*
+import io.circe.syntax.*
+
+/**
+ * The server's response to a completion/complete request
+ */
+final case class CompleteResult(values: List[String], total: Option[Int], hasMore: Option[Boolean]) extends Result
+
+object CompleteResult:
+  given Decoder[CompleteResult] = Decoder.instance { cursor =>
+    for {
+      values <- cursor.get[List[String]]("completion.values")
+      total <- cursor.get[Option[Int]]("completion.total")
+      hasMore <- cursor.get[Option[Boolean]]("completion.hasMore")
+    } yield CompleteResult(values, total, hasMore)
+  }
+
+  given Encoder[CompleteResult] = Encoder.instance { result =>
+    Json
+      .obj(
+        "completion" -> Json.obj(
+          "values" -> result.values.asJson,
+          "total" -> result.total.asJson,
+          "hasMore" -> result.hasMore.asJson
+        )
+      )
+      .dropNullValues
+  }
