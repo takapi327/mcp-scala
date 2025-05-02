52b7ff469c7abde3e118be8a33aa623365a6385e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-05-03 01:31:55 +0900

    Replace CreateMessageRequest to request pacakge

--- module/mcp-scala-schema/src/main/scala/mcp/schema/Request.scala
+++ module/mcp-scala-schema/src/main/scala/mcp/schema/Request.scala
@@ -11,70 +11,12 @@ import io.circe.syntax.*
 
 import mcp.schema.McpSchema.{
   CompleteArgument,
-  ContextInclusionStrategy,
-  ModelPreferences,
   PromptOrResourceReference,
-  SamplingMessage
 }
 
 import mcp.schema.request.*
 
 object Request:
-
-  /**
-   * A request from the server to sample an LLM via the client. The client has full discretion over which model to select. The client should also inform the user before beginning sampling, to allow them to inspect the request (human in the loop) and decide whether to approve it.
-   */
-  final case class CreateMessageRequest(
-    messages:         List[SamplingMessage],
-    modelPreferences: Option[ModelPreferences],
-    systemPrompt:     Option[String],
-    includeContext:   Option[ContextInclusionStrategy],
-    temperature:      Option[String],
-    maxTokens:        Int,
-    stopSequences:    Option[Int],
-    metaData:         Option[Json]
-  ) extends Request:
-    override def method: Method = Method.METHOD_NOTIFICATION_MESSAGE
-  object CreateMessageRequest:
-    given Decoder[CreateMessageRequest] = Decoder.instance { cursor =>
-      for {
-        messages         <- cursor.get[List[SamplingMessage]]("messages")
-        modelPreferences <- cursor.get[Option[ModelPreferences]]("modelPreferences")
-        systemPrompt     <- cursor.get[Option[String]]("systemPrompt")
-        includeContext   <- cursor.get[Option[ContextInclusionStrategy]]("includeContext")
-        temperature      <- cursor.get[Option[String]]("temperature")
-        maxTokens        <- cursor.get[Int]("maxTokens")
-        stopSequences    <- cursor.get[Option[Int]]("stopSequences")
-        metaData         <- cursor.get[Option[Json]]("metaData")
-      } yield CreateMessageRequest(
-        messages,
-        modelPreferences,
-        systemPrompt,
-        includeContext,
-        temperature,
-        maxTokens,
-        stopSequences,
-        metaData
-      )
-    }
-
-    given Encoder[CreateMessageRequest] = Encoder.instance { create =>
-      Json
-        .obj(
-          "method" -> create.method.asJson,
-          "params" -> Json.obj(
-            "messages"         -> create.messages.asJson,
-            "modelPreferences" -> create.modelPreferences.asJson,
-            "systemPrompt"     -> create.systemPrompt.asJson,
-            "includeContext"   -> create.includeContext.asJson,
-            "temperature"      -> create.temperature.asJson,
-            "maxTokens"        -> create.maxTokens.asJson,
-            "stopSequences"    -> create.stopSequences.asJson,
-            "metaData"         -> create.metaData.asJson
-          )
-        )
-        .dropNullValues
-    }
 
   final case class CompleteRequest(ref: PromptOrResourceReference, argument: CompleteArgument) extends Request:
     override def method: Method = Method.METHOD_COMPLETION_COMPLETE
