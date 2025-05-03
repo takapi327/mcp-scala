95d47bb0adda24af3d188417c585400d9f8b5eba
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-05-03 18:54:21 +0900

    Replace LoggingMessageNotification to notification pacakge

--- module/mcp-scala-schema/src/main/scala/mcp/schema/Notification.scala
+++ module/mcp-scala-schema/src/main/scala/mcp/schema/Notification.scala
@@ -13,35 +13,6 @@ import mcp.schema.notification.Notification
 
 object Notification:
 
-  /**
-   * Notification of a log message passed from server to client. If no logging/setLevel request has been sent from the client, the server MAY decide which messages to send automatically.
-   */
-  final case class LoggingMessageNotification(level: LoggingLevel, logger: Option[String], data: Json)
-    extends Notification:
-    override def method: Method = Method.METHOD_NOTIFICATION_MESSAGE
-  object LoggingMessageNotification:
-    given Decoder[LoggingMessageNotification] = Decoder.instance { cursor =>
-      for {
-        method <- cursor.get[Method]("method").map(_ == Method.METHOD_NOTIFICATION_MESSAGE)
-        level  <- cursor.get[LoggingLevel]("level")
-        logger <- cursor.get[Option[String]]("logger")
-        data   <- cursor.get[Json]("data")
-      } yield
-        if method then LoggingMessageNotification(level, logger, data)
-        else throw new Exception("Invalid method for LoggingMessageNotification")
-    }
-
-    given Encoder[LoggingMessageNotification] = Encoder.instance { message =>
-      Json.obj(
-        "method" -> message.method.asJson,
-        "params" -> Json.obj(
-          "level"  -> message.level.asJson,
-          "logger" -> message.logger.asJson,
-          "data"   -> message.data.asJson
-        )
-      )
-    }
-
   final case class RootsListChangedNotification() extends Notification:
     override def method: Method = Method.METHOD_NOTIFICATION_ROOTS_LIST_CHANGED
   object RootsListChangedNotification:
